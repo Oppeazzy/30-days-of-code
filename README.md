1c726b243a5421cb3cf3f1846108849b946b8f89
Aderibigbe Opeyemi <aderibigbeopeyemi11@gmail.com>
2025-06-28 01:24:30 -0700

    Add Day 0 solutions

--- README.md
+++
@@ -1,2 +0,0 @@
-# 30-days-of-code
-My solutions for HackerRank's 30 Days of Code challenge.
---
+++ solution.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string input_string;
+    getline(cin, input_string);
+    cout << "Hello, World." << endl;
+    cout << input_string << endl;
+    return 0;
+}
